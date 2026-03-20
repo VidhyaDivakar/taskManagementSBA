29afdbcd001ad771d3a548c70205310c54bf4c7e
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-20 13:51:43 -0400

    Added draft files with pseudocode

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reflections
+
+* 
+* Challenges faced during the project.
+* How you approached solving those challenges.
+* What you would improve if given more time.
---
+++ app.js
@@ -0,0 +1,29 @@
+//Task Name
+//Category
+//Deadline
+//Status - Inprogress, Completed, Overdue
+// Auto Update task status Overdue
+//Filter task by status or category
+//Save task data using local storage 
+
+// Task 1 Adding New Tasks
+
+// Task Name, Category, Deadline, Status as object
+
+let taskRoutine = {
+taskName:"",
+category:""
+deadline:"",
+status: "",
+task: function(){
+
+},
+};
+
+let taskArr = [];
+
+// Task 2 Displaying the Task List
+
+//Task 3 Updating Task Status
+
+// Hold Task 5 unitll today evening Persisting Task Data with Local Storage
