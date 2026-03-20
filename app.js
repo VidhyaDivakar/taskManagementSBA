4097c5258ed2d4384d037098b055ed89b4e369de
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-20 16:37:58 -0400

    Added object to the array

--- app.js
+++ app.js
@@ -8,21 +8,31 @@
 
 // Task 1 Adding New Tasks
 
+
+
+let taskArray = [];
+let taskRoutine = { taskName: "", category: "", deadline: "", status: "" };
+let btnAddtask = document.getElementById("btnAddtask");
 // Task Name, Category, Deadline, Status as object
+// assigned the object to the variable and added it to a function
 
-let taskRoutine = {
-taskName:"",
-category:""
-deadline:"",
-status: "",
-task: function(){
+btnAddtask = addEventListener("click", function addtask() {
+    taskArray = taskRoutine.Value;
+    if (taskName === "" || category === "" || deadline === "") {
+        alert("Please enter an item.")
+        return;
+    }
+});
 
-},
-};
+
+
+
 
 let taskArr = [];
 
 // Task 2 Displaying the Task List
+
+displayTask()
 
 //Task 3 Updating Task Status
 
