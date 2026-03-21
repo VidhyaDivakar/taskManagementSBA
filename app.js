7257090d9c2631ef87ab551f7899c21765be3909
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-21 07:28:05 -0400

    Add tasks in array draft 2

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-//Task Name
-//Category
-//Deadline
-//Status - Inprogress, Completed, Overdue
-// Auto Update task status Overdue
-//Filter task by status or category
-//Save task data using local storage 
-
 // Task 1 Adding New Tasks
 
 
 // Adding Task Name, Category, Deadline, Status as object
 let taskArray = [];
-let taskRoutine = { taskName: "", category: "", deadline: "", status: "" };
+// let taskRoutine = { taskName: "", category: "", deadline: "", status: "" };
 let btnAddtask = document.getElementById("btnAddtask");
 let taskName = document.getElementById("taskName");
 let category = document.getElementById("category");
@@ -20,30 +12,42 @@ let status = document.getElementById("status");
 
 // assigned the object to the array and added it to a function
 
-btnAddtask = addEventListener("click", function addtask() {
+btnAddtask.addEventListener("click", function addtask() {
 
     let task = {
         name: taskName.value,
         category: category.value,
         deadline: deadline.value,
-        status: status.value,
+        status: status.value
     };
-    if (taskName === "" || category === "" || deadline === "" || status === "") {
-        alert("Please enter an item.")
+    if (taskName.value === "" || category.value === "" || deadline.value === "" || status.value === "") {
+        alert("Please enter all values.")
         return;
     }
+    console.log("The task is", task);
     taskArray.push(task);
     displayArray();
-    // to clear the input fields
-    taskNameInput.value = "";
-    categoryInput.value = "";
-    deadlineInput.value = "";
-    statusInput.value = "In Progress";
+    // to clear
+    //  the input fields
+    taskName.value = "";
+    category.value = "";
+    deadline.value = "";
+    status.value = "In Progress";
 
     console.log(taskArray);
-});
+    });
 
+// Display Array
+function displayArray() {
+    const list = document.getElementById("taskArray");
+    list.innerHTML = ""; // clear previous list
 
+    taskArray.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${task.name} | ${task.category} | ${task.deadline} | ${task.status}`;
+        list.appendChild(li);
+    });
+}
 
 
 
@@ -51,7 +55,6 @@ btnAddtask = addEventListener("click", function addtask() {
 
 // Task 2 Displaying the Task List
 
-displayTask()
 
 //Task 3 Updating Task Status
 
