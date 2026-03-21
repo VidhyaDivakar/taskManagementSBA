96fb1483f4908c0bb27f9fd5159b5480b4c68794
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-21 15:52:53 -0400

    Update Status

--- app.js
+++ app.js
@@ -11,31 +11,30 @@ let deadline = document.getElementById("deadline");
 let status = document.getElementById("status");
 
 // assigned the object to the array and added it to a function
-
 btnAddtask.addEventListener("click", function addtask() {
-
     let task = {
         name: taskName.value,
         category: category.value,
         deadline: deadline.value,
         status: status.value
     }
-    if (taskName.value === "" || category.value === "" || deadline.value === "" || status.value === "") {
-        alert("Please enter all values.")
-        return;
-    }
 
-    taskArray.push(task);
-    displayArray();
-    taskName.value = "";  // to clear the input fields
-    category.value = "";
-    deadline.value = "";
-    status.value = "In Progress";
 
-    console.log(taskArray);
+if (taskName.value === "" || category.value === "" || deadline.value === "" || status.value === "") {
+    alert("Please enter all values.")
+    return;
+}
+taskArray.push(task);
+displayArray();
+taskName.value = "";  // to clear the input fields
+category.value = "";
+deadline.value = "";
+status.value = "In Progress";
+
+console.log(taskArray);
 });
 
-// Display Array
+// Task 2 function to Display Array
 function displayArray() {
     const list = document.getElementById("taskArray");
     list.innerHTML = ""; // clear previous list
@@ -47,13 +46,37 @@ function displayArray() {
     });
 }
 
+// //Task 3 Updating Task Status 
+function updateStatus (){
+const currentDate = new Date();
+const taskDeadline = new Date(deadline.value);
+
+if (taskDeadline < currentDate){
+    status.value = "Overdue";
+    console.log("Past due");
+} else {
+    status.value = "In Progress"
+}
+}
+
+// function updateStatus() {
+//     const dueDate = new Date('2025-10-15');
+//     const d = new Date();
+
+//     if (deadline.value > d) {
+//         status.value == Overdue; change itto not editable
+//         console.log('Past due');
+//     }
+// } else {
+//     status.value !== Overdue;
+// }
 
 
 
 
-// Task 2 Displaying the Task List
 
+// Automatically check each task’s deadline and mark tasks as “Overdue” if the current date has passed the deadline.
+// Update the displayed task list whenever a task’s status changes.
 
-//Task 3 Updating Task Status
 
 // Hold Task 5 unitll today evening Persisting Task Data with Local Storage
